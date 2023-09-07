98867dc3aa37032cc7e518f4b10abc2a102a738c
TDDY <jiataizang@gmail.com>
2023-09-07 18:16:45 +0800

    #

--- JUDGE GIRL/THE C PROGRAMMING/COMPARISON AND LOGIC/8 Triangle Length.c
+++ JUDGE GIRL/THE C PROGRAMMING/COMPARISON AND LOGIC/8 Triangle Length.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
 
-int x1,y1,x2,y2,x3,y3,x4,y4;
-int flag;
-
-int crs(int xa,int ya,int xb,int yb){
-    return xa*yb-xb*ya;
-}
+int a,b,c;
 
 signed main(){
-    scanf("%d%d%d%d%d%d%d%d", &x1,&y1,&x2,&y2,&x3,&y3,&x4,&y4);
-    flag=crs(x1,y1,x2,y2);
-    if(flag*crs(x2,y2,x3,y3)<0) printf("0");
-    else if(flag*crs(x3,y3,x4,y4)<0) printf("0");
-    else if(flag*crs(x4,y4,x1,y1)<0) printf("0");
-    else printf("1");
-
+    scanf("%d%d%d", &a,&b,&c);
+    if(a+b>c && a+c>b && b+c>a) printf("1");
+    else printf("0");
 }
