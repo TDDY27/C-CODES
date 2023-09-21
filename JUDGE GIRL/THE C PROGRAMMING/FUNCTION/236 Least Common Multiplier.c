d42fb2dfd36ac8874bc116cc14de49b23997c1e0
TDDY <jiataizang@gmail.com>
2023-09-21 21:07:44 +0800

    #

---
+++ JUDGE GIRL/THE C PROGRAMMING/FUNCTION/236 Least Common Multiplier.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int gcd(int a,int b){
+    while(a>0 && b>0){
+        if(a>b) a-=b;
+        else b-=a;
+    }
+    if(a>b) return a;
+    return b;
+}
+
+signed main(){
+    int rec,num;
+    scanf("%d", &rec);
+    while(scanf("%d", &num)!=EOF){
+        int tmp;
+        tmp=gcd(rec,num);
+        rec=num*rec/tmp;
+    }
+    printf("%d", rec);
+}
