d92b97fa812e2ccca38707bf651e96d043feb25d
TDDY <jiataizang@gmail.com>
2023-10-18 11:00:07 +0800

    #

--- JUDGE GIRL/THE C PROGRAMMING/CHARACTER/75 Count Letters.c
+++ JUDGE GIRL/THE C PROGRAMMING/CHARACTER/75 Count Letters.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 signed main(){
-    char rec[150];
-    scanf("%s", &rec);
-    printf("%s", rec);
+    char rec[]={};
+    scanf("%s", rec);
+	int len=sizeof(rec)/sizeof(char);
+	
+	printf("%ld\n", sizeof(rec));
+	printf("%c", rec[0]);
+	printf("%d", len);
+	int ans[50]={};
+	for(int i=0;i<len;i++){
+		if(rec[i]>='A' && rec[i]<'a') ans[rec[i]-'A']++;
+		else if(rec[i]>='a' && rec[i]<='z') ans[rec[i]-'a']++;		}	
+	for(int i=0;i<26;i++) printf("%d\n", ans[i]); 
+	printf("%d", (int)rec[0]-'a');
+	printf("%d", 5);
 }
+
