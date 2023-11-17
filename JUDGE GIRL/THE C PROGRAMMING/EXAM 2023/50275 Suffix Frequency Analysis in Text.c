991a404195fdc524ce24510ca6cf6eb6b246fbaa
TDDY <jiataizang@gmail.com>
2023-11-17 17:12:57 +0800

    Create 50275 Suffix Frequency Analysis in Text.c

---
+++ JUDGE GIRL/THE C PROGRAMMING/EXAM 2023/50275 Suffix Frequency Analysis in Text.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+ 
+signed main(){
+    char lne[100000];
+    char* suf[]={"ed","tion","ness","ful","ing","ly","able","ous","ish","ive","less","ism","ist","ize","ment","ship","ary","ical","ence","hood"};
+    for(int i=0;i<20-1;i++){
+        for(int j=i+1;j<20;j++){
+            if(strcmp(suf[i],suf[j])>0){
+                char *tmp=suf[i];
+                suf[i]=suf[j];
+                suf[j]=tmp;
+            }
+        }
+    }
+ 
+    int rec[30]={0};
+    while(gets(lne)){
+        char *tmp;
+        char del[]=" .,!?;:";
+        tmp=strtok(lne,del);
+        while(tmp!=NULL){
+            char chk[100000];
+            strcpy(chk,tmp);
+            for(int i=0;i<20;i++){
+                char* pos=strstr(chk,suf[i]);
+                if(pos!=NULL && (pos-chk+strlen(suf[i]) )==strlen(chk)) rec[i]++;
+            }
+            tmp=strtok(NULL,del);
+        }
+    }
+ 
+    for(int i=0;i<20;i++)
+        if(rec[i]>0) printf("%s:\nFrequency: %d\n", suf[i],rec[i]);
+}
